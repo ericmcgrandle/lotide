e81f47416865856eae4d38e52b999b1411403502
Eric McGrandle <ericmcgrandle@gmail.com>
2020-09-21 20:37:18 +0000

    init npm, refactored headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,23 @@
+const { assert } = require('chai');
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello"]), "Hello");
-assertEqual(head([]), "Hello");
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("returns '5' for ['5', '6', '7']", () => {
+    assert.strictEqual(head(['5', '6', '7']), '5'); 
+  });
+
+  it("returns '5' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'); 
+  });
+
+});
